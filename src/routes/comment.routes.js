c240002e3cfe979ee5901c1091ea9010e4f2c365
Eniola <eniolaa177@gmail.com>
2026-02-19 01:18:22 +0100

    added a comment system

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -3,20 +3,31 @@ import {
   createComment,
   getCommentsByPost,
   deleteComment,
+  updateComment,
 } from "../controllers/comment.controller.js";
 import authMiddleware from "../middleware/auth.middleware.js";
+import { validateRequest } from "../middleware/validateRequest.js";
+import {
+  createCommentValidator,
+  updateCommentValidator,
+} from "../Validator/comment.validator.js";
 
 const router = express.Router();
 
 /**
  * Create a comment (protected)
  */
-router.post("/", authMiddleware, createComment);
+router.post("/", authMiddleware, createCommentValidator, validateRequest, createComment);
 
 /**
  * Get comments for a post (public)
  */
 router.get("/post/:postId", getCommentsByPost);
+
+/**
+ * Update a comment (protected - owner only)
+ */
+router.put("/:id", authMiddleware, updateCommentValidator, validateRequest, updateComment);
 
 /**
  * Delete a comment (protected)
