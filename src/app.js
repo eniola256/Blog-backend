1e2bba5b9e1bb1cd81fc497c202dac0a779ca7e1
Eniola <eniolaa177@gmail.com>
2026-03-11 11:38:49 +0100

    transformed image 2

--- src/app.js
+++ src/app.js
@@ -37,8 +37,8 @@ if (!fs.existsSync(uploadsDir)) {
 }
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({ limit: "20mb", extended: true }));
 app.use('/uploads', express.static(uploadsDir));
 app.use(helmet());
 app.use("/api", apiLimiter);
