2db3507dfd1d1d554813a748b349975c3e585908
Eniola <eniolaa177@gmail.com>
2026-03-15 23:54:43 +0100

    fix

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -220,6 +220,8 @@ export const getAllPosts = async (req, res) => {
 export const updatePost = async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
+    console.log("UPDATE BODY featuredImage:", req.body.featuredImage);
+    console.log("UPDATE BODY image:", req.body.image);
 
     if (!post) {
       return res.status(404).json({ message: "Post not found" });
